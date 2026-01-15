c453691eaca07aa6708b098c3a412d69673c4505
Pannnomaly <supawith007@gmail.com>
2026-01-15 09:40:26 +0700

    feat(conatctus): add CRUD api about contact-us page

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import cookieParser from "cookie-parser";
 import helmet from "helmet";
 import cors from "cors";
 import { router as apiRoutes } from "./routes/index.js";
-import { centralizedError as errorHandling } from "./middlewares/errorHandling.js";
+import { centralizedError, notFoundError } from "./middlewares/errorHandling.js";
 
 export const app = express();
 
@@ -17,8 +17,7 @@ const corsOptions = {
     origin: [
         "http://localhost:5173",
             "http://localhost:5174",
-                "http://localhost:5175",
-                    "https://frontend-react-app-ecru.vercel.app",     
+                "http://localhost:5175",     
     ],
     credentials: true,
 };
@@ -29,4 +28,5 @@ app.use(cookieParser());
 
 app.use("/api", apiRoutes);
 
-app.use(errorHandling);
+app.use(notFoundError);
+app.use(centralizedError);
